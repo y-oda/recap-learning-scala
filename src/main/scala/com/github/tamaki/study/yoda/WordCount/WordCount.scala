4a3a4a660cca9ceaac81d10364b636ef9b57389f
y-oda <yohei.oda@bizreach.co.jp>
2015-06-05 09:27:39 +0900

    テストをコメントアウト

--- src/main/scala/com/github/tamaki/study/yoda/WordCount/WordCount.scala
+++ src/main/scala/com/github/tamaki/study/yoda/WordCount/WordCount.scala
@@ -13,6 +13,7 @@ class WordCount {
     lines.flatMap(str => str.split(" ")).groupBy(identity).mapValues(_.size)
   }
 
+  /** テスト
   val lines = List("apple banana", "orange apple mango", "kiwi papaya orange", "mango orange muscat apple")
   val fruitsCounts = countFruitsFromLines(lines)
   if (fruitsCounts == Map("banana" -> 1, "muscat" -> 1, "orange" -> 3, "mango" -> 2, "apple" -> 3, "kiwi" -> 1, "papaya" -> 1)) {
@@ -20,4 +21,5 @@ class WordCount {
   } else {
     println("不正解")
   }
+    */
 }
