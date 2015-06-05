d498e9d3c346dbf3ab19089266f340b5c2c375c3
y-oda <yohei.oda@bizreach.co.jp>
2015-06-05 09:18:47 +0900

    ワードカウントの回答を追加（小田）

--- src/main/scala/com/github/tamaki/study/yoda/WordCount/WordCount.scala
+++ src/main/scala/com/github/tamaki/study/yoda/WordCount/WordCount.scala
@@ -1,8 +1,23 @@
-package com.github.tamaki.study.yoda.WordCount
+package com.github.tamaki.study.yoda.wordcount
 
 /**
  * Created by yohei.oda on 2015/06/04.
  */
-class WordCount {
+//------------------------------------------------------
+// ワードカウント問題
+// https://gist.github.com/j5ik2o/7210762
+//------------------------------------------------------
 
+class WordCount {
+  def countFruitsFromLines(lines: List[String]): Map[String, Int] = {
+    lines.flatMap(str => str.split(" ")).groupBy(identity).mapValues(_.size)
+  }
+
+  val lines = List("apple banana", "orange apple mango", "kiwi papaya orange", "mango orange muscat apple")
+  val fruitsCounts = countFruitsFromLines(lines)
+  if (fruitsCounts == Map("banana" -> 1, "muscat" -> 1, "orange" -> 3, "mango" -> 2, "apple" -> 3, "kiwi" -> 1, "papaya" -> 1)) {
+      println("正解")
+  } else {
+    println("不正解")
+  }
 }
