6a60f27e29e5a4263af9f00bfb5a69a5f4bfea86
y-oda <yohei.oda@bizreach.co.jp>
2015-06-16 10:19:43 +0900

    tailrec

--- src/main/scala/com/github/tamaki/study/yoda/tailrec/Rec.scala
+++ src/main/scala/com/github/tamaki/study/yoda/tailrec/Rec.scala
@@ -1,10 +1,13 @@
 package com.github.tamaki.study.yoda.tailrec
 
+import scala.annotation.tailrec
+
 /**
  * Created by yohei.oda on 2015/06/16.
  */
 object Rec {
   def sum(n:Int):Int={
+    @tailrec
       def loop(n:Int, acc:Int):Int={
         n match {
           case 0 => acc
